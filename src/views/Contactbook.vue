e826d0891153026d6b39575e90272ec8a42f9238
Batch <learncode10002003@gmail.com>
2025-06-20 05:44:36 +0700

    Tao trang cap nhat lien he

--- src/views/Contactbook.vue
+++ src/views/Contactbook.vue
@@ -33,6 +33,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh</span
+          >
+        </router-link>
       </div>
     </div>
   </div>
@@ -115,8 +125,8 @@ export default {
 </script>
 
 <style scoped>
-    .page {
-        text-align: left;
-        max-width: 750px;
-    }
+.page {
+  text-align: left;
+  max-width: 750px;
+}
 </style>
